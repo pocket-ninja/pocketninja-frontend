211d5dffae21cab0d2c473e2a054313720ab9f6e
sroik <vasili.kazhanouski@gmail.com>
2022-04-01 13:10:22 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -24,6 +24,25 @@ export default function Home() {
 
         <link rel="icon" href="/favicon.ico" />
         <script data-ad-client="ca-pub-1680034082323710" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    
+    <!-- Meta Pixel Code -->
+<script>
+  !function(f,b,e,v,n,t,s)
+  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+  n.queue=[];t=b.createElement(e);t.async=!0;
+  t.src=v;s=b.getElementsByTagName(e)[0];
+  s.parentNode.insertBefore(t,s)}(window, document,'script',
+  'https://connect.facebook.net/en_US/fbevents.js');
+  fbq('init', '1295075370978677');
+  fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+  src="https://www.facebook.com/tr?id=1295075370978677&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Meta Pixel Code -->
+  
       </Head>
 
       <main>
